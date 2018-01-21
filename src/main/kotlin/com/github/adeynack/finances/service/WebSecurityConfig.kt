482f13f74c4161631be32bc651561c6f4fb28f15
David Dufour-Boivin <david.dufour.boivin@gmail.com>
2018-01-21 20:46:06 +0100

    From tutorial @ http://www.baeldung.com/securing-a-restful-web-service-with-spring-security#javaconfig

--- src/main/kotlin/com/github/adeynack/finances/service/WebSecurityConfig.kt
+++ src/main/kotlin/com/github/adeynack/finances/service/WebSecurityConfig.kt
@@ -1,51 +1,97 @@
 package com.github.adeynack.finances.service
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties
 import org.springframework.context.annotation.Configuration
-import org.springframework.core.annotation.Order
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.core.Authentication
 import org.springframework.security.core.AuthenticationException
-import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint
+import org.springframework.security.web.AuthenticationEntryPoint
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache
 import org.springframework.stereotype.Component
+import org.springframework.util.StringUtils
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
+/**
+ * From http://www.baeldung.com/securing-a-restful-web-service-with-spring-security#javaconfig
+ */
 @Configuration
 @EnableWebSecurity
-@Order(SecurityProperties.BASIC_AUTH_ORDER)
 class WebSecurityConfig(
-    private val authenticationEntryPoint: MyBasicAuthenticationEntryPoint
+    private val restAuthenticationEntryPoint: RestAuthenticationEntryPoint,
+    private val authenticationSuccessHandler: MySavedRequestAwareAuthenticationSuccessHandler
 ) : WebSecurityConfigurerAdapter() {
 
     override fun configure(auth: AuthenticationManagerBuilder) {
         auth.inMemoryAuthentication()
-            .withUser("user").password("pass").authorities("USER")
+            .withUser("admin").password("pass").roles("ADMIN")
+            .and()
+            .withUser("user").password("pass").roles("USER")
     }
 
     override fun configure(http: HttpSecurity) {
-        http.authorizeRequests()
-            .antMatchers("/api/health").permitAll()
-            .anyRequest().authenticated()
+        http
+            .csrf().disable()
+            .exceptionHandling()
+            .authenticationEntryPoint(restAuthenticationEntryPoint)
             .and()
-            .httpBasic().authenticationEntryPoint(authenticationEntryPoint)
-//        http.addFilterAfter(CustomFilter(), BasicAuthenticationFilter::class.java)
+            .authorizeRequests()
+            .antMatchers("/**").authenticated()
+            .and()
+            .formLogin()
+            .successHandler(authenticationSuccessHandler)
+            .failureHandler(SimpleUrlAuthenticationFailureHandler())
+            .and()
+            .logout()
     }
 }
 
+/**
+ * From http://www.baeldung.com/securing-a-restful-web-service-with-spring-security#ch_3_2
+ */
 @Component
-class MyBasicAuthenticationEntryPoint : BasicAuthenticationEntryPoint() {
+class RestAuthenticationEntryPoint : AuthenticationEntryPoint {
 
     override fun commence(request: HttpServletRequest, response: HttpServletResponse, authException: AuthenticationException) {
-        response.addHeader("WWW-Authenticate", """Basic realm=$realmName""")
-        response.status = HttpServletResponse.SC_UNAUTHORIZED
-        response.writer.println("HTTP Status 401 - ${authException.message}")
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized")
     }
 
-    override fun afterPropertiesSet() {
-        realmName = "Finances"
-        super.afterPropertiesSet()
+}
+
+/**
+ * From http://www.baeldung.com/securing-a-restful-web-service-with-spring-security#ch_3_4
+ */
+@Component
+class MySavedRequestAwareAuthenticationSuccessHandler : SimpleUrlAuthenticationSuccessHandler() {
+
+    private val requestCache = HttpSessionRequestCache()
+
+    override fun onAuthenticationSuccess(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        authentication: Authentication
+    ) {
+        val savedRequest = requestCache.getRequest(request, response)
+        if (savedRequest == null) {
+            clearAuthenticationAttributes(request)
+            return
+        }
+
+        val targetUrlParam = targetUrlParameter
+        if (
+            isAlwaysUseDefaultTargetUrl ||
+            (targetUrlParam != null && StringUtils.hasText(request.getParameter(targetUrlParam)))
+        ) {
+            requestCache.removeRequest(request, response)
+            clearAuthenticationAttributes(request)
+            return
+        }
+
+        clearAuthenticationAttributes(request)
     }
+
 }

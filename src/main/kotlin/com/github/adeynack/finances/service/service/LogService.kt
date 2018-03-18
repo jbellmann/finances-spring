054e8fe7da08ebde8813f4747cb9a2f4690a58e2
David Dufour-Boivin <david.dufour.boivin@gmail.com>
2018-03-18 23:36:16 +0100

    WIP

--- src/main/kotlin/com/github/adeynack/finances/service/service/LogService.kt
+++ src/main/kotlin/com/github/adeynack/finances/service/service/LogService.kt
@@ -1,12 +1,13 @@
 package com.github.adeynack.finances.service.service
 
-import org.apache.log4j.LogManager
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
 import org.springframework.stereotype.Component
 import kotlin.reflect.KClass
 
 @Component
 class LogService {
 
-    fun <T : Any> getLogger(clazz: KClass<T>) = LogManager.getLogger(clazz.java)
+    fun <T : Any> getLogger(clazz: KClass<T>): Logger = LogManager.getLogger(clazz.java)
 
 }
